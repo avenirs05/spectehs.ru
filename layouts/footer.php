f19e75faaaa83b030175fe587a5aa7f72ed802cf
avenirs05 <avenirs05@gmail.com>
2018-02-06 17:19:05 +0200

    last

---
+++ layouts/footer.php
@@ -0,0 +1,44 @@
+<div class="container-fluid header">
+	<nav class="row navbar navbar-dark bg-dark footer">
+		<div class="col">
+			<span><b>Тел.:</b> +380 99 291 89 60</span> 
+		</div>
+		<div class="w-100"></div>
+		<div class="col">
+			<span><b>email:</b> info@uberline.in.ua</span>
+		</div>
+		<div class="w-100"></div>
+		<div class="col logo d-flex align-items-center">
+			Сайт сделан в<span class="d-none d-lg-block">&nbsp;веб-студии</span>
+			<a href="http://smartfox.su/" target="_blank" style="display: block; text-decoration: none;"><div style="padding: 0px 5px; display: flex; align-items: center; border-radius: 3px;">
+					<!-- <img class="logo-img" src="/imgs/logo-fox.png" height="18" alt="">&nbsp; -->
+					<span class="logo-text" style="font-size: 14px; color: rgb(251, 140, 0);">SMART FOX</span>
+				</div>
+			</a>
+		</div>
+		<div class="w-100"></div>
+		<div class="col text-center d-none d-lg-block">
+			<span class="copyright">&copy; uberline.in.ua, 2017</span>
+		</div>
+		<div class="col text-left d-lg-none">
+			<span class="copyright">&copy; uberline.in.ua, 2017</span>
+		</div>
+	</nav>
+</div>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+
+<!-- My Own Js-scripts -->
+<script src="js/main.js"></script>
+
+<!-- Маска ввода -->
+<script src="js/jquery.maskedinput.js" type="text/javascript"></script>
+
+</body>
+</html>
